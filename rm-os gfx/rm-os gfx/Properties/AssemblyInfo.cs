57720de37d2e83fd2259b1e07d1c89483e15957e
remona-minett <50187357+remona-minett@users.noreply.github.com>
2020-08-16 16:55:25 -0700

    adjustment of releases

--- rm-os gfx/rm-os gfx/Properties/AssemblyInfo.cs
+++ rm-os gfx/rm-os gfx/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("rm-os wfa test")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("rm-os")]
+[assembly: AssemblyDescription("A handy task launcher.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("rm-os wfa test")]
+[assembly: AssemblyCompany("Remona-Minett")]
+[assembly: AssemblyProduct("rm-os graphics edition")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
