844ef98b575a4ebdb93d7057002c0f3c2b89ea52
remona-minett <50187357+remona-minett@users.noreply.github.com>
2020-08-15 18:12:59 -0700

    framework

---
+++ rm-os/rm-os/Kernel/KInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace rm_os.Kernel
+{
+    class KInfo
+    {
+        public static void PrintTargetRuntime()
+        {
+            var stats = new
+            {
+                RuntimeInformation.OSDescription,
+                RuntimeInformation.OSArchitecture,
+                RuntimeInformation.ProcessArchitecture,
+                RuntimeInformation.FrameworkDescription
+            };
+            Console.WriteLine("OS Platform is : " + stats.OSDescription);
+            Console.WriteLine("OS Architecture is : " + stats.OSArchitecture);
+            Console.WriteLine("Framework is " + stats.FrameworkDescription);
+            if (stats.ProcessArchitecture == Architecture.Arm)
+            {
+                Console.WriteLine("ARM process.");
+            }
+            else if (stats.ProcessArchitecture == Architecture.Arm64)
+            {
+                Console.WriteLine("ARM64 process.");
+            }
+            else if (stats.ProcessArchitecture == Architecture.X64)
+            {
+                Console.WriteLine("x64 process.");
+            }
+            else if (stats.ProcessArchitecture == Architecture.X86)
+            {
+                Console.WriteLine("x86 process.");
+            }
+        }
+        public static void kernelInfo()
+        {
+            string kver = "1.0.0"; // update version number here
+            Console.Clear();
+            Console.WriteLine("System Info:\n");
+            Console.WriteLine("Kernel Version: " + kver);
+            PrintTargetRuntime();
+        }
+    }
+}
---
+++ rm-os/rm-os/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using rm_os.Kernel;
+
+namespace rm_os
+{
+    class Program
+    {
+        static void Main()
+        {
+            KInfo.kernelInfo();
+            Console.ReadKey(); // eof hang to read output
+        }
+    }
+}
